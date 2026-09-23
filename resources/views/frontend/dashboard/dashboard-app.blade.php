ab6ec5610e75fd9297e483a3fa26db8cca14a44c
BlanchardIlela <blancilela@gmail.com>
2026-09-23 05:06:34 +0100

    Deconnxion User

--- resources/views/frontend/dashboard/dashboard-app.blade.php
+++ resources/views/frontend/dashboard/dashboard-app.blade.php
@@ -32,8 +32,11 @@
                                         <a class="nav-link" href="#wishlist-tab"><i class="fi-rs-heart mr-10"></i> Wishlist</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="login.html"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
+                                        <a class="nav-link" onclick="event.preventDefault(); $('.form-logout').submit()" href="login.html"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
                                     </li>
+                                    <form class="form-logout" action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                    </form>
                                 </ul>
                             </div>
                         </div>
