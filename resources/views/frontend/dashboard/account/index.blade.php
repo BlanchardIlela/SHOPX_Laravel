359f7dd693645e9fc23b75c8a80a54afef80295c
BlanchardIlela <blancilela@gmail.com>
2026-09-23 13:24:56 +0100

    Le formulaire de modification - Modifions les identités de User - Ajoutant la méthode PUT

--- resources/views/frontend/dashboard/account/index.blade.php
+++ resources/views/frontend/dashboard/account/index.blade.php
@@ -8,15 +8,19 @@
             </div>
             <div class="card-body p-0">
                 <p>You can edit your account details here.</p>
-                <form method="post" name="enq">
+                <form method="post" action="{{ route('profiles.update') }}">
+                    @csrf
+                    @method('PUT')
                     <div class="row mt-30">
                         <div class="form-group col-md-12">
-                            <label>First Name <span class="required">*</span></label>
-                            <input required="" class="form-control" name="name" type="text" />
+                            <label>Name <span class="required">*</span></label>
+                            <input required="" class="form-control" name="name" type="text" value="{{ auth('web')->user()->name }}" />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
                         <div class="form-group col-md-12">
                             <label>Email Address <span class="required">*</span></label>
-                            <input required="" class="form-control" name="email" type="email" />
+                            <input required="" class="form-control" name="email" type="email"  value="{{ auth('web')->user()->email }}" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
                         </div>
                         <div class="col-md-12">
                             <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit"
@@ -33,19 +37,24 @@
             </div>
             <div class="card-body p-0">
                 <p>You can change your password.</p>
-                <form method="post" name="enq">
+                <form method="post" action="{{ route('password.update') }}">
+                    @csrf
+                    @method('PUT')
                     <div class="row mt-30">
                         <div class="form-group col-md-12">
                             <label>Current Password <span class="required">*</span></label>
-                            <input required="" class="form-control" name="password" type="password" />
+                            <input required="" class="form-control" name="current_password" type="password" />
+                            <x-input-error :messages="$errors->get('current_password')" class="mt-2" />
                         </div>
                         <div class="form-group col-md-12">
                             <label>New Password <span class="required">*</span></label>
-                            <input required="" class="form-control" name="npassword" type="password" />
+                            <input required="" class="form-control" name="password" type="password" />
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
                         <div class="form-group col-md-12">
                             <label>Confirm Password <span class="required">*</span></label>
-                            <input required="" class="form-control" name="cpassword" type="password" />
+                            <input required="" class="form-control" name="password_confirmation" type="password" />
+                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                         </div>
                         <div class="col-md-12">
                             <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit"
